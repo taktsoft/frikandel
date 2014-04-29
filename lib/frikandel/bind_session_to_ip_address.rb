1ca68852b511fee3716685d55b6dc452a1ce302c
Michael Nowak <nowak@taktsoft.com>
2014-04-29 13:40:33 +0200

    reset-session-only-once: added code for new features, fixed tests for new feature

--- lib/frikandel/bind_session_to_ip_address.rb
+++ lib/frikandel/bind_session_to_ip_address.rb
@@ -10,10 +10,11 @@ module Frikandel
   private
 
     def validate_session_ip_address
-      if session.key?(:ip_address) && !ip_address_match_with_current?(session[:ip_address])
+      if session.key?(:ip_address) && !ip_address_match_with_current?
         on_invalid_session
       elsif !session.key?(:ip_address)
-        reset_session
+        reset_session_with_bind_session_to_ip_address_style
+        persist_session_ip_address
       end
     end
 
@@ -25,9 +26,21 @@ module Frikandel
       request.remote_ip
     end
 
-    def ip_address_match_with_current?(ip_address)
-      current_ip_address == ip_address
+    def ip_address_match_with_current?
+      session[:ip_address] == current_ip_address
     end
 
+    def reset_session_with_bind_session_to_ip_address_style
+      unless @_frikandel_did_reset_session
+        stored_ttl = session[:ttl]
+        stored_max_ttl = session[:max_ttl]
+
+        reset_session
+        @_frikandel_did_reset_session = true
+
+        session[:ttl] = stored_ttl if stored_ttl.present?
+        session[:max_ttl] = stored_max_ttl if stored_max_ttl.present?
+      end
+    end
   end
 end
--- lib/frikandel/limit_session_lifetime.rb
+++ lib/frikandel/limit_session_lifetime.rb
@@ -11,16 +11,36 @@ module Frikandel
   private
 
     def validate_session_timestamp
-      if session.key?(:ttl) && session.key?(:max_ttl) && (session[:ttl] < Frikandel::Configuration.ttl.ago || session[:max_ttl] < Time.now)
+      if session.key?(:ttl) && session.key?(:max_ttl) && (reached_ttl? || reached_max_ttl?)
         on_invalid_session
-      elsif !session.key?(:ttl) && !session.key?(:max_ttl)
-        reset_session
+      elsif !session.key?(:ttl) || !session.key?(:max_ttl)
+        reset_session_with_limit_session_lifetime_style
+        persist_session_timestamp
       end
+    end
+
+    def reached_ttl?
+      session[:ttl] < Frikandel::Configuration.ttl.ago
+    end
+
+    def reached_max_ttl?
+      session[:max_ttl] < Time.now
     end
 
     def persist_session_timestamp
       session[:ttl] = Time.now
       session[:max_ttl] ||= Frikandel::Configuration.max_ttl.from_now
+    end
+
+    def reset_session_with_limit_session_lifetime_style
+      unless @_frikandel_did_reset_session
+        stored_ip_address = session[:ip_address]
+
+        reset_session
+        @_frikandel_did_reset_session = true
+
+        session[:ip_address] = stored_ip_address if stored_ip_address.present?
+      end
     end
   end
 end
